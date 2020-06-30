3481cf9aa61d27b9ebcb35a1629dcd41485c607c
kolakarmahadevi <47476578+kolakarmahadevi@users.noreply.github.com>
2020-06-30 12:15:45 +0530

    Update MyfirstName.java
    second commit

--- JavaOneProject/src/MyfirstName.java
+++ JavaOneProject/src/MyfirstName.java
@@ -10,6 +10,8 @@ public class MyfirstName {
 			System.out.println(a);//10 because second condition is not checked
 			System.out.println(a < b&a++ < c);//false && true = false
 			System.out.println(a);//11 because second condition is checked
+			System.out.println("helli");
+			
 			}
 
 	}
